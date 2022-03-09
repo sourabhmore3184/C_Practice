02ca3a093a2092716f24e48484cbce6af70eeec2
sourabhmore3184 <98277622+sourabhmore3184@users.noreply.github.com>
2022-03-09 22:27:05 +0530

    Add files via upload

---
+++ C++/Que22_Read_Any_Digit_And_Display_Word.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=0;
+
+    cout<<"Enter Any Digit : ";
+    cin>>num;
+
+    if(num==1)
+    {
+        cout<<"One";
+    }
+    else if(num==2)
+    {
+        cout<<"Two";
+    }
+    else if(num==3)
+    {
+        cout<<"Three";
+    }
+    else if(num==4)
+    {
+        cout<<"Four";
+    }
+    else if(num==5)
+    {
+        cout<<"Five";
+    }
+    else if(num==6)
+    {
+        cout<<"six";
+    }
+    else if(num==7)
+    {
+        cout<<"Seven";
+    }
+    else if(num==8)
+    {
+        cout<<"Eight";
+    }
+    else if(num==9)
+    {
+        cout<<"Nine";
+    }
+    else if(num==10)
+    {
+        cout<<"Ten";
+    }
+    return 0;
+}
+
---
+++ C++/Que23_Display_Month.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=0;
+
+    cout<<"Enter Month Number : ";
+    cin>>num;
+
+    if(num==1)
+    {
+        cout<<"January";
+    }
+    else if(num==2)
+    {
+        cout<<"February";
+    }
+    else if(num==3)
+    {
+        cout<<"march";
+    }
+    else if(num==4)
+    {
+        cout<<"April";
+    }
+    else if(num==5)
+    {
+        cout<<"may";
+    }
+    else if(num==6)
+    {
+        cout<<"June";
+    }
+    else if(num==7)
+    {
+        cout<<"July";
+    }
+    else if(num==8)
+    {
+        cout<<"August";
+    }
+    else if(num==9)
+    {
+        cout<<"September";
+    }
+    else if(num==10)
+    {
+        cout<<"October";
+    }
+    else if(num==11)
+    {
+        cout<<"November";
+    }
+    else if(num==12)
+    {
+        cout<<"December";
+    }
+    return 0;
+}
