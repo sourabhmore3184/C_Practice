0fe5c11cb55fd7d22128d793e065b12119fff978
sourabhmore3184 <98277622+sourabhmore3184@users.noreply.github.com>
2022-03-10 23:10:50 +0530

    Add files via upload

---
+++ C++/Que24_Read_Any_Month_Number_In_Integer_Display_the_Number_of_Day_In_Month.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=0;
+
+    cout<<"Enter Month Number : ";
+    cin>>num;
+
+    if(num==1)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else if(num==2)
+    {
+        cout<<"Month Have 28 Days";
+    }
+    else if(num==3)
+    {
+        cout<<"month Have 31 Days";
+    }
+    else if(num==4)
+    {
+        cout<<"Month Have 30 Days";
+    }
+    else if(num==5)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else if(num==6)
+    {
+        cout<<"Month Have 30 Days";
+    }
+    else if(num==7)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else if(num==8)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else if(num==9)
+    {
+        cout<<"Month Have 30 Days";
+    }
+    else if(num==10)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else if(num==11)
+    {
+        cout<<"Month Have 30 Days";
+    }
+    else if(num==12)
+    {
+        cout<<"Month Have 31 Days";
+    }
+    else
+    {
+        cout<<"Invalid Month Number";
+    }
+    return 0;
+}
+
