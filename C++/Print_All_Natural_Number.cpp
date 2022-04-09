945137ced0d24b2d3266065b0736c0b1b8c4227a
sourabhmore3184 <98277622+sourabhmore3184@users.noreply.github.com>
2022-04-09 23:07:36 +0530

    Add files via upload

---
+++ C++/Print_All_Natural_Number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+     cout<<"Enter Number";
+     cin>> n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<" "<<i;
+       // cin>>n;
+
+    }
+    return 0;
+}
---
+++ C++/Print_All_Natural_Number_In_Reverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+     cout<<"Enter Number";
+     cin>>n;
+    for(i=n;i>=1;i--)
+    {
+        cout<<" "<<i;
+       // cin>>n;
+
+    }
+    return 0;
+}
+
