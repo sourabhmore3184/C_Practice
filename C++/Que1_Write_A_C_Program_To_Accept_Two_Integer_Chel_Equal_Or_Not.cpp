c876b76fe1b59192b50669db5ddea9dfa1fcd9a2
sourabhmore3184 <98277622+sourabhmore3184@users.noreply.github.com>
2022-05-08 10:44:14 +0530

    Add files via upload

---
+++ C++/Que1_Write_A_C_Program_To_Accept_Two_Integer_Chel_Equal_Or_Not.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1=0,n2=0;
+
+    cout<<"Enter first Number ";
+    cin>>n1;
+
+    cout<<"Enter Two Number ";
+    cin>>n2;
+
+    if(n1==n2)
+    {
+        cout<<n1<<" and "<<n2<<" Are Equal"<<endl;
+    }
+    else
+    {
+        cout<<n1<<" and "<<n2<<" Are Not Equal"<<endl;
+    }
+}
